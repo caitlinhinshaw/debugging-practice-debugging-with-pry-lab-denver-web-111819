114ad61d4fee00aaa4176b586e3cfc4b9e5aec98
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-18 02:14:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,7 @@
 def snake_it_up(string)
   string_array = string.split("")
   if string_array[0] == "s"
-  new_string = "s"*10 
-  binding.pry
+  new_string = "s"*10 + string
   else
   string
   end
